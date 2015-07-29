96dc52be609043c547375720c88a75413044fc13
Ash McKenzie <ash@the-rebellion.net>
2015-07-30 09:31:19 +1000

    Load CLI a bit later

--- lib/salt/clt.rb
+++ lib/salt/clt.rb
@@ -2,9 +2,9 @@ require 'json'
 
 require 'salt/clt/version'
 require 'salt/clt/config'
-require 'salt/clt/cli'
 require 'salt/clt/errors'
 require 'salt/clt/api'
+require 'salt/clt/cli'
 
 module Salt
   module CLT
